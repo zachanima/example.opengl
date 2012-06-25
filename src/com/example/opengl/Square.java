6f9c914cc96d05dec2b0d4a372a7b771d94a8684
Zach Ewert <zachanima@gmail.com>
2012-06-26 01:41:14 +0200

    Use nearest neighbour for texture mag filter

--- src/com/example/opengl/Square.java
+++ src/com/example/opengl/Square.java
@@ -67,7 +67,7 @@ public class Square {
     gl.glGenTextures(1, textures, 0);
     gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
     gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
-    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
     GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
     
     bitmap.recycle();
