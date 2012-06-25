aa7a814fabb07df287e742429c8da4505e177fc0
Zach Ewert <zachanima@gmail.com>
2012-06-26 01:19:22 +0200

    Further fix whitespace

--- src/com/example/opengl/GLRenderer.java
+++ src/com/example/opengl/GLRenderer.java
@@ -20,23 +20,21 @@ public class GLRenderer implements Renderer {
     gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
     gl.glLoadIdentity();
     gl.glTranslatef(0.0f, 0.0f, -5.0f);
-    
+
     square.draw(gl);
   }
 
 
   @Override
   public void onSurfaceChanged(GL10 gl, int width, int height) {
-    if (height == 0) {
-      height = 1;
-    }
-    
+    if (height == 0) { height = 1; } // Avoid division by 0.
+
     gl.glViewport(0, 0, width, height);
     gl.glMatrixMode(GL10.GL_PROJECTION);
     gl.glLoadIdentity();
-    
+
     GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 0.1f, 100.0f);
-    
+
     gl.glMatrixMode(GL10.GL_MODELVIEW);
     gl.glLoadIdentity();
   }
