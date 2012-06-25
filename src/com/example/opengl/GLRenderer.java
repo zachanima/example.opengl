35c82b47ff068bf8beea772127db3199807dfd5c
Zach Ewert <zachanima@gmail.com>
2012-06-26 00:17:17 +0200

    Use OpenGL renderer

---
+++ src/com/example/opengl/GLRenderer.java
@@ -0,0 +1,22 @@
+package com.example.opengl;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class GLRenderer implements Renderer {
+
+  @Override
+  public void onDrawFrame(GL10 gl) {
+  }
+
+  @Override
+  public void onSurfaceChanged(GL10 gl, int width, int height) {
+  }
+
+  @Override
+  public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+  }
+
+}
--- src/com/example/opengl/Run.java
+++ src/com/example/opengl/Run.java
@@ -1,13 +1,34 @@
 package com.example.opengl;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Run extends Activity {
-    /** Called when the activity is first created. */
+    private GLSurfaceView glSurfaceView;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        // Full screen.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        // Initiate OpenGL view, create instance with this activity.
+        glSurfaceView = new GLSurfaceView(this);
+        
+        // Set renderer as main.
+        glSurfaceView.setRenderer(new GLRenderer());
+        setContentView(glSurfaceView);
+    }
+    
+    @Override
+    protected void onResume() {
+      super.onResume();
+      glSurfaceView.onResume();
     }
 }
