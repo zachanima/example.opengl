34582c604c956d0c11684cb3a4be99cfe5e0610f
Zach Ewert <zachanima@gmail.com>
2012-06-26 15:50:54 +0200

    Revert to 8734f69c511463b53496d54f271cf5451535b75c

--- src/com/example/opengl/GameRenderer.java
+++ src/com/example/opengl/GameRenderer.java
@@ -25,25 +25,25 @@ public class GameRenderer implements Renderer {
     final long ms = time - this.time;
     this.time = time;
     
-    ship.update(ms);
+    /*ship.update(ms);
     for (int i = 0; i < stars.size(); i++) {
       stars.get(i).update(ms);
-    }
+    }*/
   }
 
 
   @Override
   public void onDrawFrame(GL10 gl) {
-    update();
+    /*update();
     
     gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
     gl.glLoadIdentity();
     gl.glTranslatef(0.0f, 0.0f, -20.0f);
     
-    ship.draw(gl);
     for (int i = 0; i < stars.size(); i++) {
       stars.get(i).draw(gl);
     }
+    ship.draw(gl);*/
   }
 
 
@@ -64,14 +64,14 @@ public class GameRenderer implements Renderer {
 
   @Override
   public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-     ship.loadGLTexture(gl, this.context);
+     //ship.loadGLTexture(gl, this.context);
      
      gl.glEnable(GL10.GL_TEXTURE_2D);
      gl.glShadeModel(GL10.GL_SMOOTH);
      gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
      gl.glClearDepthf(1.0f);
-     gl.glEnable(GL10.GL_DEPTH_TEST);
-     gl.glDepthFunc(GL10.GL_LEQUAL);
+     gl.glEnable(GL10.GL_BLEND);
+     gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
      gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
   }
   
