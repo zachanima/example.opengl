c5d1d6fdd24c48bae878573a46dc26af509e3ae3
Zach Ewert <zachanima@gmail.com>
2012-06-26 14:24:06 +0200

    Move stars into background

--- src/com/example/opengl/Star.java
+++ src/com/example/opengl/Star.java
@@ -5,21 +5,21 @@ import javax.microedition.khronos.opengles.GL10;
 public class Star extends Square {
   public Star() {
     r[0] = (float)Math.random() * 50.0f - 25.0f;
-    r[1] = (float)Math.random() * 20.0f - 10.0f;
+    r[1] = (float)Math.random() * 25.0f - 12.5f;
   }
   
   
   public void update(long ms) {
     r[1] -= v;
-    if (r[1] < -10.0f) {
-      r[1] = 10.0f;
+    if (r[1] < -12.5f) {
+      r[1] = 12.5f;
     }
   }
   
   
   public void draw(GL10 gl) {
     gl.glPushMatrix();
-    gl.glTranslatef(r[0], r[1], 0.0f);
+    gl.glTranslatef(r[0], r[1], -1.0f);
     gl.glScalef(0.05f, 0.1f, 0.05f);
     super.draw(gl);
     gl.glPopMatrix();
