6c0e7f071d4edbba0b408e45a9863905b279db40
Zach Ewert <zachanima@gmail.com>
2012-06-26 07:39:45 +0200

    Cap horisontal star position to -10..10

--- src/com/example/opengl/Star.java
+++ src/com/example/opengl/Star.java
@@ -5,14 +5,14 @@ import javax.microedition.khronos.opengles.GL10;
 public class Star extends Square {
   public Star() {
     r[0] = (float)Math.random() * 50.0f - 25.0f;
-    r[1] = (float)Math.random() * 50.0f - 25.0f;
+    r[1] = (float)Math.random() * 20.0f - 10.0f;
   }
   
   
   public void update(long ms) {
     r[1] -= v;
-    if (r[1] < -25.0f) {
-      r[1] = 25.0f;
+    if (r[1] < -10.0f) {
+      r[1] = 10.0f;
     }
   }
   
