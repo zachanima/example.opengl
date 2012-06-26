74f9fb048adb87d28bc9a080eed6b949afd9c431
Zach Ewert <zachanima@gmail.com>
2012-06-26 14:29:47 +0200

    Enable blending

--- src/com/example/opengl/GameRenderer.java
+++ src/com/example/opengl/GameRenderer.java
@@ -70,6 +70,8 @@ public class GameRenderer implements Renderer {
      gl.glShadeModel(GL10.GL_SMOOTH);
      gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
      gl.glClearDepthf(1.0f);
+     gl.glEnable(GL10.GL_BLEND);
+     gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA)
      gl.glEnable(GL10.GL_DEPTH_TEST);
      gl.glDepthFunc(GL10.GL_LEQUAL);
      gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
