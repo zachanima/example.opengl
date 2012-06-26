5e890e5717dd9090faa3201b35c084f968523b0a
Zach Ewert <zachanima@gmail.com>
2012-06-26 05:29:13 +0200

    Rotate less when moving

--- src/com/example/opengl/Ship.java
+++ src/com/example/opengl/Ship.java
@@ -12,7 +12,7 @@ public class Ship extends Square {
     gl.glPushMatrix();
     gl.glScalef(2.0f, 1.0f, 1.0f);
     gl.glTranslatef(r[0], r[1], 0.0f);
-    gl.glRotatef(v * 16, 0.0f, 1.0f, 0.0f);
+    gl.glRotatef(v * 8, 0.0f, 1.0f, 0.0f);
     super.draw(gl);
     gl.glPopMatrix();
   }
